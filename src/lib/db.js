b8e2de8c8d25e87f7d9ae966bc5ee5ccc5d5a123
Yoo Daewhan <iok112@naver.com>
2025-06-20 22:50:44 +0900

    db작동완료

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,10 @@
-// src/lib/db.js
 import mysql from "mysql2/promise";
 
-export const db = mysql.createPool({
-  host: "ec2-3-38-180-108.ap-northeast-2.compute.amazonaws.com",
+const pool = mysql.createPool({
+  host: "ec2-13-125-238-80.ap-northeast-2.compute.amazonaws.com",
   user: "ydh960823",
   password: "Adbtmddyd2!",
   database: "chat-api",
 });
 
-export default db;
+export default pool;
